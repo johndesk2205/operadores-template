69488ffe0e9ab7b61a42d89aa3d63956f6c17ac1
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-16 11:33:59 -0300

    Resolução dos exercicios

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,59 @@
-//CODIGO AQUI
+//
+let saldo = 0
+//Adicione 22.40 ao seu saldo
+saldo = saldo + 22.40
+
+console.log("Saldo depois de adicionar", saldo)
+
+//Gaste 12.34 do seu saldo
+
+saldo = saldo - 12.34
+
+console.log("Saldo depois de gastar:", saldo.toFixed(2))
+
+//Divida seu dinheiro com seu irmão
+
+saldo = saldo/2
+
+console.log("A divisão por 2 deu:", saldo.toFixed(2))
+
+//Aplicar o saldo num investimento que render 3.40 mais
+
+saldo = saldo * 3.40
+
+console.log("Seu novo saldo é:", saldo.toFixed(2))
+
+//Dividir o dinheiro em 3 partes iguais e pegar o resto pra gastar
+
+saldo = saldo%3
+
+console.log(`Sobrou: ${saldo.toFixed(2)}`)
+
+//ordem de precedencia
+valor = 2
+
+valor = valor *(2 + 2)
+
+console.log(valor)
+
+//Testes de igual
+console.log(5===5)
+console.log(5!==5)
+console.log(5!=="5")
+console.log(typeof 5 === typeof 20)
+console.log(typeof "5"=== typeof "cinco")
+
+//Testes de menor e maior
+
+let x = 10
+let y = 22
+
+//primeiro igual ao segundo
+console.log(`x é igual a y? ${x===y}`)
+//primeiro menor ou igual ao segundo
+console.log(x<=y)
+//primeiro maior que o segundo
+console.log(x>y)
+//primeiro menor que o segundo
+console.log(x<y)
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,11 @@
 // EXERCICIO DE FIXAÇÃO
 
+const minhaIdade = prompt("Qual a sua idade?")
+
+const idadeAmigo = prompt("Qual a idade do seu amigo?")
+
+console.log(`Sua idade é maior que a do seu melhor amigo? ${minhaIdade>idadeAmigo}`)
+
+console.log(`A difereça de idades é: ${minhaIdade-idadeAmigo}`)
+
+
